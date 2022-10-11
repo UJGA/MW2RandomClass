94bbe45b1455488b14c6da81d9d0094bef479e51
Blake <blakeriding@gmail.com>
2022-10-11 14:58:37 -0600

    Updates

---
+++ JS/scripts.js
@@ -0,0 +1,235 @@
+var primarysArray = [
+  {
+    name: "AK-47",
+  },
+];
+
+var secondarysArray = [
+  {
+    name: ".50 GS",
+  },
+];
+
+var perk1Array = [
+  {
+    name: "Cold Blooded",
+  },
+];
+
+var perk2Array = [
+  {
+    name: "Ghost",
+  },
+];
+
+var perk3Array = [
+  {
+    name: "Amped",
+  },
+];
+
+var lethalsArray = [
+  {
+    name: "Claymore",
+  },
+];
+
+var tacticalsArray = [
+  {
+    name: "Flash Grenade",
+  },
+];
+
+var fieldUpgradesArray = [
+  {
+    name: "Dead Silence",
+    image: "upgrade/DeadSilence.png",
+  },
+];
+
+function random_item() {
+  document.getElementById("primaryAtt1").innerHTML = "";
+  document.getElementById("primaryAtt2").innerHTML = "";
+  document.getElementById("primaryAtt3").innerHTML = "";
+  document.getElementById("primaryAtt4").innerHTML = "";
+  document.getElementById("primaryAtt5").innerHTML = "";
+  document.getElementById("primaryAtt6").innerHTML = "";
+  document.getElementById("primaryAtt7").innerHTML = "";
+  document.getElementById("primaryAtt8").innerHTML = "";
+  document.getElementById("primaryAtt9").innerHTML = "";
+
+  document.getElementById("secondaryAtt1").innerHTML = "";
+  document.getElementById("secondaryAtt2").innerHTML = "";
+  document.getElementById("secondaryAtt3").innerHTML = "";
+  document.getElementById("secondaryAtt4").innerHTML = "";
+  document.getElementById("secondaryAtt5").innerHTML = "";
+  document.getElementById("secondaryAtt6").innerHTML = "";
+  document.getElementById("secondaryAtt7").innerHTML = "";
+  document.getElementById("secondaryAtt8").innerHTML = "";
+  document.getElementById("secondaryAtt9").innerHTML = "";
+
+  var element = document.getElementById("classContent");
+  element.classList.add("classContent");
+
+  //Primary
+  var randomPrimaryNumber =
+    Math.floor(Math.random() * primarysArray.length) + 0;
+  var randomPrimary = primarysArray[randomPrimaryNumber];
+  //console.log("The Random primary number is-------------------------" + randomPrimaryNumber);
+  // console.log("The name is -----------------------------------------" + randomPrimary.name);
+  //console.log("The image is -----------------------------------------" + randomPrimary.image);
+  document.getElementById("primary").innerHTML = randomPrimary.name;
+  document.getElementById("primaryH3").innerHTML = "Primary";
+  document.getElementById("primaryAttH3").innerHTML = "Primary Attachments";
+  // document.getElementById("primaryAttH2").innerHTML = "None";
+
+  //Secondary
+  var randomSecondary =
+    secondarysArray[Math.floor(Math.random() * secondarysArray.length)];
+  document.getElementById("secondary").innerHTML = randomSecondary.name;
+  document.getElementById("secondaryH3").innerHTML = "Secondary";
+
+  //Perk 1
+  var randomPerk1 = perk1Array[Math.floor(Math.random() * perk1Array.length)];
+  document.getElementById("perk1").innerHTML = randomPerk1.name;
+  document.getElementById("perk1H3").innerHTML = "Perk 1";
+
+  //Perk 2
+  var randomPerk2 = perk2Array[Math.floor(Math.random() * perk2Array.length)];
+  document.getElementById("perk2").innerHTML = randomPerk2.name;
+  document.getElementById("perk2H3").innerHTML = "Perk 2";
+
+  // const randomSecondary = null;
+
+  if (randomPerk2.name === "Overkill") {
+    while (
+      randomSecondary === null ||
+      randomSecondary.name === randomPrimary.name
+    ) {
+      // TODO Set 'randomSecondary' to a random primary weapon
+      var randomSecondary =
+        primarysArray[Math.floor(Math.random() * primarysArray.length)];
+      document.getElementById("secondary").innerHTML = randomSecondary.name;
+      document.getElementById("secondaryH3").innerHTML = "Secondary";
+    }
+
+    var randomSecondary =
+      primarysArray[Math.floor(Math.random() * primarysArray.length)];
+    document.getElementById("secondary").innerHTML = randomSecondary.name;
+    document.getElementById("secondaryH3").innerHTML = "Secondary";
+  }
+
+
+  //Perk 3
+  var randomPerk3 = perk3Array[Math.floor(Math.random() * perk3Array.length)];
+  document.getElementById("perk3").innerHTML = randomPerk3.name;
+  document.getElementById("perk3H3").innerHTML = "Perk 3";
+
+  //Lethal
+  var randomLethal =
+    lethalsArray[Math.floor(Math.random() * lethalsArray.length)];
+  document.getElementById("lethal").innerHTML = randomLethal.name;
+  document.getElementById("lethalH3").innerHTML = "Lethal";
+
+  //Tactical
+  var randomTactical =
+    tacticalsArray[Math.floor(Math.random() * tacticalsArray.length)];
+  document.getElementById("tactical").innerHTML = randomTactical.name;
+  document.getElementById("tacticalH3").innerHTML = "Tactical";
+
+  //Field Upgrade
+  var randomUpgrade =
+    fieldUpgradesArray[Math.floor(Math.random() * fieldUpgradesArray.length)];
+  document.getElementById("upgrade").innerHTML = randomUpgrade.name;
+  document.getElementById("upgradeH3").innerHTML = "Field Upgrade";
+
+  //Moves Button and Revmoves top text
+
+  document.getElementById("myTitle").innerHTML = "";
+  document.getElementById("goAgain").innerHTML =
+    '<button id="mybtn" class="btn btn-primary" onclick="random_item();">Generate Again</button><br><br>';
+  document.getElementById("playButton").innerHTML = "";
+
+  // document.getElementById("primary").innerHTML = "";
+  document.getElementById("primaryAtt1").innerHTML = "";
+  document.getElementById("primaryAtt2").innerHTML = "";
+  document.getElementById("primaryAtt3").innerHTML = "";
+  document.getElementById("primaryAtt4").innerHTML = "";
+  document.getElementById("primaryAtt5").innerHTML = "";
+  document.getElementById("primaryAtt6").innerHTML = "";
+  document.getElementById("primaryAtt7").innerHTML = "";
+  document.getElementById("primaryAtt8").innerHTML = "";
+  document.getElementById("primaryAtt9").innerHTML = "";
+
+  // document.getElementById("secondary").innerHTML = "";
+  document.getElementById("secondaryAtt1").innerHTML = "";
+  document.getElementById("secondaryAtt2").innerHTML = "";
+  document.getElementById("secondaryAtt3").innerHTML = "";
+  document.getElementById("secondaryAtt4").innerHTML = "";
+  document.getElementById("secondaryAtt5").innerHTML = "";
+  document.getElementById("secondaryAtt6").innerHTML = "";
+  document.getElementById("secondaryAtt7").innerHTML = "";
+  document.getElementById("secondaryAtt8").innerHTML = "";
+  document.getElementById("secondaryAtt9").innerHTML = "";
+
+  document.getElementById("secondaryAttH3").innerHTML = "Secondary Attachments";
+  // document.getElementById("secondaryAttH2").innerHTML = "Coming";
+
+  // Call to weapon functions
+  switch (randomPrimary.name) {
+    case "AK-47":
+      ak47();
+      break;
+    case "Kilo 141":
+      kilo141();
+      break;
+  }
+ 
+
+  //-------------------------------------------------------- Secondaries
+  switch (randomSecondary.name) {
+    case ".50 GS":
+      gs50();
+      break;
+    case "X16":
+      x16();
+      break;
+  }
+
+  //--------------------------------------------------------------Overkill
+  switch (randomSecondary.name) {
+    case "M4A1":
+      m4a1o();
+      break;
+    case "Kilo 141":
+      kilo();
+      break;
+  }
+ 
+  //-------------------------------------------------------------------------
+
+  document.getElementById("killstreaks").innerHTML = "Killstreaks";
+  console.log("Hit the above line");
+
+  document.getElementById("ks1").innerHTML = "";
+  document.getElementById("ks2").innerHTML = "";
+  document.getElementById("ks3").innerHTML = "";
+  document.getElementById("ks4").innerHTML = "";
+  document.getElementById("ks5").innerHTML = "";
+  document.getElementById("ks6").innerHTML = "";
+  document.getElementById("ks7").innerHTML = "";
+  document.getElementById("ks8").innerHTML = "";
+  document.getElementById("ks9").innerHTML = "";
+  document.getElementById("ks10").innerHTML = "";
+  document.getElementById("ks11").innerHTML = "";
+  document.getElementById("ks12").innerHTML = "";
+  document.getElementById("ks13").innerHTML = "";
+  document.getElementById("ks14").innerHTML = "";
+  document.getElementById("ks15").innerHTML = "";
+  document.getElementById("ks16").innerHTML = "";
+  document.getElementById("ks17").innerHTML = "";
+  document.getElementById("ks18").innerHTML = "";
+  document.getElementById("ks19").innerHTML = "";
+
+  randomKillstreaks();
+}
